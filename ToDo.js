74d25e7dcb1c4b4d2c445f8737a9a842554dae72
SergeVernez <sergevernez.pro@gmail.com>
2024-12-04 11:44:30 +0100

    ajout fonctions

--- ToDo.js
+++ ToDo.js
@@ -1,2 +1,24 @@
-// ---création de la variable---
-let taskList = document.getElementById('taskList')
+// ---création de la variable taskList---
+let taskList = document.getElementById("taskList"); //--recuperation de l'element qui possed l'ID tasklisit (liste non ordonée)
+
+// ---création de la fonction addTask---
+function addTask() {
+	// ---création de variable taskInpunt---
+	let taskInpunt = document.getElementById("taskInput"); // element avec l'id taskInput
+	// ---création de la variable taskText---
+	let taskText = taskInpunt.value;
+	// la variable sera egale à la valeur de taskInput (ce qui va remplir la tâche)
+
+	// --- création de la condition if---
+	if (taskText === "") {
+		return;
+	} // si taskText est strictement égale (===) à vide aolrs on ne fait rien
+
+	// --- création de variable pour la li---
+	let li = document.createElement("li"); // pour créer l'element li (la tâche)
+
+	li.innerHTML = taskText;
+
+	taskList.appendChild(li);
+	taskInpunt.value = ""; // Une fois la tâche saisie laisse l'input vide
+}
