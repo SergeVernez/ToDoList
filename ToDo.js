8d0d6e4464f0301409fe79eaa6f02935b0754ff8
SergeVernez <sergevernez.pro@gmail.com>
2024-12-04 12:06:20 +0100

    fix modif

--- ToDo.js
+++ ToDo.js
@@ -5,14 +5,14 @@ let taskList = document.getElementById("taskList"); //--recuperation de l'elemen
 function addTask() {
 	// ---création de variable taskInpunt---
 	let taskInpunt = document.getElementById("taskInput"); // element avec l'id taskInput
+
 	// ---création de la variable taskText---
-	let taskText = taskInpunt.value;
-	// la variable sera egale à la valeur de taskInput (ce qui va remplir la tâche)
+	let taskText = taskInpunt.value; // la variable sera egale à la valeur de taskInput (ce qui va remplir la tâche)
 
 	// --- création de la condition if---
 	if (taskText === "") {
 		return;
-	} // si taskText est strictement égale (===) à vide aolrs on ne fait rien
+	} // si taskText est strictement égale(===) à vide alors on ne fait rien
 
 	// --- création de variable pour la li---
 	let li = document.createElement("li"); // pour créer l'element li (la tâche)
@@ -21,4 +21,5 @@ function addTask() {
 
 	taskList.appendChild(li);
 	taskInpunt.value = ""; // Une fois la tâche saisie laisse l'input vide
+
 }
